8fde9de3d7c9093c584dedd1700adcbce125a4be
Nicole-1013 <ngalvan1013@gmail.com>
2024-08-05 13:07:43 -0700

    Update MadLibs.py

--- MadLibs.py
+++ MadLibs.py
@@ -1,3 +1,10 @@
+#Description: This program is a little funny mad libs story meant to
+#ask the user for different words to complete the story
+#Author: Nicole Galvan
+#Date: 08/05/24
+
+
+print('This is the start of your Mad Libs, enjoy!')
 
 noun_1 = input("noun: ")
 adjective_1 = input("adjective: ")
@@ -10,10 +17,12 @@ adjective_2 = input("adjective: ")
 verb_2 = input("past tense verb: ")
 silly_word = input("silly word: ")
 
+#This completes the user input for the Mad Lib
+
 nounToPlural = noun_1 + "s"
 print("Here you go!")
 
-MadLib = '''Once uppon a time, in the {} land of {}, there was
+MadLib = '''Once upon a time, in the {} land of {}, there was
 a {}. This {} wasn't like any other {}; it had {} {} across
 the sky. One day, it decided to visit a place where there were
 exactly {} {} {}. The {} {} {} at the sight of them and
