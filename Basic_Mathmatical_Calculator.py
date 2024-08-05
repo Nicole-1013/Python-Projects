3c8f38564e94b51f9fb28d17350251a17230f824
Nicole-1013 <ngalvan1013@gmail.com>
2024-08-05 14:31:57 -0700

    Update Basic_Mathmatical_Calculator.py

---
+++ Basic_Mathmatical_Calculator.py
@@ -0,0 +1,14 @@
+#Description: This is a basic calculator program that will perform addition
+#subtraction, division, and mulitplication with two user inputs
+#Author: Nicole Galvan
+#Date: 08/05/24
+
+print("---------- BASIC CALCULATOR ----------")
+print("--------Enter two number inputs--------")
+numberOne = int(input("Enter: "))
+numberTwo = int(input("Enter: "))
+
+addition_variable = numberOne + numberTwo
+subtraction_variable = numberOne - numberTwo
+multiplication_variable = numberOne * numberTwo
+division_variable = numberOne / numberTwo
