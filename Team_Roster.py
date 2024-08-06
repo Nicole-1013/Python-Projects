c203003b53b841592a97f50f233c20fb2fbf785f
Nicole-1013 <ngalvan1013@gmail.com>
2024-08-06 13:45:25 -0700

    created and completed team roster file

---
+++ Team_Roster.py
@@ -0,0 +1,10 @@
+#Description: this program will print statements including player's
+# names and jersey numbers
+#Author: Nicole Galvan
+#Date: 08/06/24
+
+for x in range(10):
+    jersey_number = input("Input Jersey Number: ")
+    player_name = input("Input Player Name: ")
+
+    print(f"Welcome to the team, {player_name}! Your team number is {jersey_number}.")
