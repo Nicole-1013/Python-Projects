6147618e11ff6a2860f127918c339acbce21d097
Nicole-1013 <ngalvan1013@gmail.com>
2024-08-06 12:58:54 -0700

    made the options easier to use, gave more options to use

--- Basic_Mathmatical_Calculator.py
+++ Basic_Mathmatical_Calculator.py
@@ -6,30 +6,39 @@
 print("---------- BASIC CALCULATOR ----------")
 print("-------Enter two number inputs-------")
 print('''        ------OPERATIONS-------
-               add
-               subtract
-               divide
-               multiply''')
+               +)add
+               -)subtract
+               /)divide
+               *)multiply
+               **)exponent
+               //)floor division''')
 print("--------------------------------------")
 
+numberOne = int(input("Enter First Number: "))
 type_of_math = input("Math Operation: ")
-numberOne = int(input("Enter Number: "))
-numberTwo = int(input("Enter Number: "))
+numberTwo = int(input("Enter Second Number: "))
 
 
 addition_variable = numberOne + numberTwo
 subtraction_variable = numberOne - numberTwo
 multiplication_variable = numberOne * numberTwo
 division_variable = numberOne / numberTwo
+exponent_variable = numberOne ** numberTwo
+floor_division_variable = numberOne // numberTwo
 
-if type_of_math == 'add':
+
+if type_of_math == '+':
     print(addition_variable)
-elif type_of_math == 'subtract':
+elif type_of_math == '-':
     print(subtraction_variable)
-elif type_of_math == 'divide':
+elif type_of_math == '/':
     print(division_variable)
-elif type_of_math == 'multiply':
+elif type_of_math == '*':
     print(multiplication_variable)
+elif type_of_math == '**':
+    print(exoponent_variable)
+elif type_of_math == '//':
+    print(floor_division_variable)
 else:
     print("INVALID MATH TYPE")
 
