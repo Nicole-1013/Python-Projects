b7437f3c74f121bdbc491abaf71360eeb1a3a2db
Nicole-1013 <ngalvan1013@gmail.com>
2024-08-06 14:15:52 -0700

    created file and made user input list of 10

---
+++ Sentence_Generator.py
@@ -0,0 +1,10 @@
+#Description: This program uses a list of nouns that iterates through funny sentences
+#Author: Nicole Galvan
+#Date: 08/06/24
+noun_list = []
+
+for x in range(9):
+    noun = input("Input a noun: ")
+    noun_list.append(noun)
+
+print(noun_list)
