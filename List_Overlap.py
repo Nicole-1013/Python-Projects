48042427d28d8b5bd35cbb46ae9236673500a867
Nicole-1013 <ngalvan1013@gmail.com>
2024-08-15 13:53:25 -0700

    used list comprehension to do the same code

--- List_Overlap.py
+++ List_Overlap.py
@@ -4,10 +4,20 @@
 
 list_one = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 list_two = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-new_list = []
 
-for x in list_one:
-    if x in list_two:
-        if x not in new_list:
-            new_list.append(x)
-print(new_list)
+new_list = [x for x in list_one if x in list_two]
+newer_list = []
+for x in new_list:
+    if x not in newer_list:
+        newer_list.append(x)
+
+
+
+#for x in list_one: #iterates through first list
+    #if x in list_two: #checks if it is in the second list also
+        #if x not in new_list: #checks if there is already a duplicate of the shared number
+            #new_list.append(x)# if not, the shared number is added to a new list
+
+
+
+print(newer_list)
