75019074aa1ce417f79c9de636de6335b276b5c3
Nicole-1013 <ngalvan1013@gmail.com>
2024-08-16 12:25:48 -0700

    made user input to create custom lists to compare

--- List_Overlap.py
+++ List_Overlap.py
@@ -1,23 +1,29 @@
+import random
 #Description: This program will find which numbers both lists have in common without duplicates, then print those numbers
 #Author: Nicole Galvan 
 #Date: 8/15/24
 
-list_one = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-list_two = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+#Asks for the length of each list so the user can create custom lists
+user_input_one = int(input("Enter the length of the first list!(Less than 100)"))
+user_input_two = int(input("Enter the length of the second list!(Less than 100)"))
 
-new_list = [x for x in list_one if x in list_two]
+#These make the initial sample list
+sample_list_one = [x for x in range(100)]
+sample_list_two= [x for x in range(100)]
+
+#takes sample lists and creates a list based on the user input
+list_one = random.sample(sample_list_one, user_input_one)
+list_two = random.sample(sample_list_two, user_input_two)
+
+#filtered common numbers into list
+new_list = [x for x in list_one if x in list_two] 
 newer_list = []
+
+#second filter getting rid of any duplicates in new_list
 for x in new_list:
     if x not in newer_list:
         newer_list.append(x)
 
-
-
-#for x in list_one: #iterates through first list
-    #if x in list_two: #checks if it is in the second list also
-        #if x not in new_list: #checks if there is already a duplicate of the shared number
-            #new_list.append(x)# if not, the shared number is added to a new list
-
-
-
 print(newer_list)
+print(list_one)
+print(list_two)
