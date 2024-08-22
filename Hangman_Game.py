c3f6e326c3d2376f9bd5920947aeaf2646d479aa
Nicole-1013 <ngalvan1013@gmail.com>
2024-08-22 14:19:48 -0700

    created initial variables and initial comments

---
+++ Hangman_Game.py
@@ -0,0 +1,9 @@
+#Description: This program will be a hangman game where the user will have to guess a word in 12 turns
+#Author: Nicole Galvan
+#Date: 8/22/24
+import random
+
+word_list = ["cheese", "duck", "book", "telephone"]
+word = random.choice(word_list)
+
+print(word)
