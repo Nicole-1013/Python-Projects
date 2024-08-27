91a8479320f82eaeb30b416d1c9b5a8c4a9943a2
Nicole-1013 <ngalvan1013@gmail.com>
2024-08-27 12:45:59 -0700

    printed first draft of the potential menu

--- Contact_list_manager.py
+++ Contact_list_manager.py
@@ -2,13 +2,21 @@
 #Author: Nicole Galvan 
 #Date: 8/27/24
 
+#Global Variable
 contacts = {}
-contact_name = ""
-contact_number = 0
 
-contact_name = input("What is the name of your new contact?")
-contact_number = input("What is the number of your new contact?")
+def add_contact():
+    contact_name = input("What is the name of your new contact?")
+    contact_number = input("What is the number of your new contact?")
 
-contacts[f"{contact_name}"] = contact_number
+    contacts[f"{contact_name}"] = contact_number
 
-print(contacts)
+    print(contacts)
+
+print("       MENU       ")
+print('''  add) add new contact   
+delete) delete contact
+view) view someone's number
+number) check if number in your list
+address) add address to contact
+note) add notes to a person's contact''')
