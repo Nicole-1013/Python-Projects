2fac67e49a62eff3d8679e3554e1b8673d5f0717
Nicole-1013 <ngalvan1013@gmail.com>
2024-08-27 14:26:26 -0700

    updated view and show functions, added address function

--- Contact_list_manager.py
+++ Contact_list_manager.py
@@ -2,31 +2,26 @@
 #if they want to view someones number, and many other functions you encounter when making a new contact
 #Author: Nicole Galvan 
 #Date: 8/27/24
-import os
 
 
 #Global Variable
 contacts = {}
 user_input = ""
-def clear_console():
-    if os.name == 'nt':
-        os.system('cls')
 
 def add_contact():
     contact_name = input("What is the name of your new contact?")
     contact_number = input("What is the number of your new contact?")
 
-    contacts[f"{contact_name}"] = [contact_number]
+    contacts[f"{contact_name}"] = [contact_number, "none"] #The "none" is the space indicated for the address which is optional to put in
 
 def print_menu():
-    print("-------MENU-------")
+    print("    -------MENU-------")
     print('''     
     add) add new contact   
     delete) delete contact
     view) view someone's contact info
     show) show contact list
     address) add address to contact
-    note) add notes to a person's contact
     quit)''')
 
 def delete_contact():
@@ -35,12 +30,18 @@ def delete_contact():
 
 def view_contact():
     contact_name = input("Name of contact")
-    print(contacts.get(f'{contact_name}'))
+    print("Number: " + contacts.get(f'{contact_name}')[0] + "| Address: " + contacts.get(f'{contact_name}')[1])
 
 def show_contacts():
     for x,y in contacts.items():
-        print(f'Name: {x} | Number: {y[0]}')
-    
+        print(f'Name: {x} | Number: {y[0]} | Address: {y[1]}')
+
+def add_address():
+    contact_name = input("Name of Contact: ")
+    address = input("Address: ")
+    contacts[f"{contact_name}"][1] = address
+
+
 while user_input != 'quit':
     print_menu()
     user_input = input("Choose Option: ").lower()
@@ -52,7 +53,9 @@ while user_input != 'quit':
         view_contact()
     elif user_input == "show":
         show_contacts()
-    clear_console()
+    elif user_input == "address":
+        add_address()
+    
     
     
 
