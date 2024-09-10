af416191e36fdf727485520f285e7ce27c11d84c
Nicole-1013 <ngalvan1013@gmail.com>
2024-09-10 14:02:25 -0700

    deleted one player function, and merged both modes into one function

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -4,49 +4,47 @@
 
 import random
 import os
-move = ["r", "p", "s"]
 play_again = 'y'
-rounds = 1
 
 def clear_console():
     if os.name == 'nt':
         os.system('cls')
 
-def one_player_game():
-    global rounds 
-    player_points = 0
-    computer_points = 0
-    while rounds <= 3:
-        player = input("Choose Action (Rock: r, Paper: p, Scissors: s)").lower()
-        computer = random.choice(move)
 
-
-
-
-def two_player_game():
-    global rounds
-    global move
+def game(second_user):
+    rounds = 1
+    move = ["r", "p", "s"]
     player_1_points = 0
     player_2_points = 0
+
+    if second_user == '2':
+        second_player = "Player 2"
+    else:
+        second_player = "Computer"
+    
     while  rounds <=3:
-        print(f"POINTS :  Player 1: {player_1_points}  |  Player 2: {player_2_points}")
+        print(f"POINTS :  Player 1: {player_1_points}  |  {second_player}: {player_2_points}")
         player_1 = input("Player 1: Choose Action (Rock: r, Paper: p, Scissors: s)").lower()
-        player_2 = input("Player 2: Choose Action (Rock: r, Paper: p, Scissors: s)").lower()
+        
+        if second_user == '2':
+            player_2 = input("Player 2: Choose Action (Rock: r, Paper: p, Scissors: s)").lower()
+        else:
+            player_2 = random.choice(move)
+
         
         if player_1 not in move or player_2 not in move:
             clear_console()
             print("INVALID RESPONSE, TRY AGAIN")
-            
             continue
 
         if player_1 == player_2:
-            print("its a Tie!")
+            print("its a tie!")
             continue
         elif (player_1 == "r" and player_2 == "p") or \
             (player_1 == "p" and player_2 == "s") or \
             (player_1 == "s" and player_2 == "r"):
             player_2_points += 1
-            print(f"Player 2 won round {rounds}!")
+            print(f"{second_player} won round {rounds}!")
         else:
             player_1_points += 1
             print(f"Player 1 won round {rounds}!")
@@ -56,24 +54,21 @@ def two_player_game():
             if player_1_points > player_2_points:
                 print("Player 1 is the Winner!")
             else:
-                print('Player 2 is the Winner!')
+                print(f'{second_player} is the Winner!')
             break
         
         rounds += 1
 
-            
-        
-                
 
-print("Welcome to Rock Paper Scissors!")
 
 while play_again == 'y':
+    print("Welcome to Rock Paper Scissors!")
     user_input = input("Two player gamemode or One player gamemode? (2,1)")
-    if user_input == '2':
-        two_player_game()
-    elif user_input == '1':
-        one_player_game()
+    if user_input == '2' or user_input == '1':
+        clear_console()
+        game(user_input)
     else:
+        clear_console()
         print("INVALID RESPONSE, TRY AGAIN")
         continue
     
